067e9bd9838ad546e9abc3a8904549a4132d0e73
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-12-24 12:07:45 +0100

    Initial commit

---
+++ src/main/java/com/green/energy/tracker/cloud/site_processor/SiteProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.green.energy.tracker.cloud.site_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteProcessorApplication.class, args);
+	}
+
+}
